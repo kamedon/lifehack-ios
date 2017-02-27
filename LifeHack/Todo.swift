07bf76d7f69b012efe53f432f9b85c7653f73bbb
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2017-02-27 11:07:25 +0900

    雛形を作成

---
+++ LifeHack/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  LifeHack
+//
+//  Created by kamei.hidetoshi on 2017/02/27.
+//  Copyright © 2017年 kamedon. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    var body: String
+}
---
+++ LifeHack/TodoRepository.swift
@@ -0,0 +1,13 @@
+//
+//  TodoRepository.swift
+//  LifeHack
+//
+//  Created by kamei.hidetoshi on 2017/02/27.
+//  Copyright © 2017年 kamedon. All rights reserved.
+//
+
+import Foundation
+
+class TodoRepository {
+    
+}
